af73d25342cd410e8f6c59f68710768dfa8dc7aa
ivan <ivan.navoznov@gmail.com>
2023-11-30 07:11:55 -0300

    Day 14 part 2

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -10,71 +10,53 @@ var maxX = allPoints.Max(p => p.X);
 var maxY = allPoints.Max(p => p.Y);
 var minX = allPoints.Min(p => p.X);
 var minY = allPoints.Min(p => p.Y);
-
-var skipLeft = 450;
-
-var map = new CellContent[maxX + 1, maxY + 2]; // one for the abyss
-
-FillMap(map, rules);
-PrintMap(map, skipLeft);
-
 var sandFallingStartPoint = new Point(500, 0);
 
-var i = 0;
-for (;; i++)
+Console.WriteLine(Part1());
+Console.WriteLine(Part2());
+
+int Part1()
 {
-    var checkNextSandPieceFallsToAbyss = CheckNextSandPieceFallsToAbyss(map, sandFallingStartPoint);
-    PrintMap(map, skipLeft);
-    if (checkNextSandPieceFallsToAbyss)
+    var map = CreateMap(maxX, maxY + 2, rules);
+
+    var i = 0;
+    for (;; i++)
     {
-        break;
-    }
-}
-
-Console.WriteLine(i);
-
-
-void FillMap(CellContent[,] map, Rule[] rules)
-{
-    foreach (var rule in rules)
-    {
-        ApplyRule(map, rule);
-    }
-}
-
-void ApplyRule(CellContent[,] map, Rule rule)
-{
-    var points = rule.Points;
-    var startPoint = points.First();
-    map[startPoint.X, startPoint.Y] = CellContent.Rock;
-
-    for (var j = 1; j < points.Length; j++)
-    {
-        var nextPoint = points[j];
-        var pointVector = nextPoint - startPoint;
-        var directionVector = GetNormalizedVector(pointVector);
-        var currentPoint = startPoint;
-        while (currentPoint != nextPoint)
+        var checkNextSandPieceFallsToAbyss = CheckNextSandPieceFallsToAbyss(map, maxX + 1, sandFallingStartPoint);
+        if (checkNextSandPieceFallsToAbyss)
         {
-            currentPoint = currentPoint + directionVector;
-            map[currentPoint.X, currentPoint.Y] = CellContent.Rock;
+            return i;
+        }
+    }
+}
+
+int Part2()
+{
+    var rockBottomLineRule = new Rule(new Point[] { new Point(0, maxY + 2), new Point(maxX + maxY, maxY + 2) });
+    var newRules = rules.Union(new[] { rockBottomLineRule }).ToArray();
+    var map = CreateMap(maxX + maxY, maxY + 1, newRules);
+    PrintMap(map, maxX + maxY, 450, 1);
+    var i = 0;
+    for (;; i++)
+    {
+        var checkNextSandPieceBlockStartPoint =
+            CheckNextSandPieceBlockStartPoint(map, maxX + maxY + 1, sandFallingStartPoint);
+        if (!checkNextSandPieceBlockStartPoint)
+        {
+            continue;
         }
 
-        startPoint = nextPoint;
+        PrintMap(map, maxX + maxY, 450, 1);
+        return i + 1;
     }
 }
 
-Vector GetNormalizedVector(Vector vector)
-{
-    return new Vector(Math.Sign(vector.X), Math.Sign(vector.Y));
-}
-
-bool CheckNextSandPieceFallsToAbyss(CellContent[,] map, Point sandFallingStartPoint)
+bool CheckNextSandPieceFallsToAbyss(CellContent[,] map, int xSize, Point sandFallingStartPoint)
 {
     var currentSandPoint = sandFallingStartPoint;
     while (true)
     {
-        var nextStepResult = TryGetSandNextStepPoint(map, currentSandPoint);
+        var nextStepResult = TryGetSandNextStepPoint(map, xSize, currentSandPoint);
         if (nextStepResult.IsFailure())
         {
             break;
@@ -95,27 +77,26 @@ bool CheckNextSandPieceFallsToAbyss(CellContent[,] map, Point sandFallingStartPo
     return false;
 }
 
-void PrintMap(CellContent[,] map, int skipLeft)
+bool CheckNextSandPieceBlockStartPoint(CellContent[,] map, int xSize, Point sandFallingStartPoint)
 {
-    for (var y = 0; y < maxY + 2; y++)
+    var currentSandPoint = sandFallingStartPoint;
+    while (true)
     {
-        for (var x = skipLeft - 1; x < maxX + 1; x++)
+        var nextStepResult = TryGetSandNextStepPoint(map, xSize, currentSandPoint);
+        if (nextStepResult.IsFailure())
         {
-            var ch = map[x, y] switch
-            {
-                CellContent.Air => '.',
-                CellContent.Rock => '#',
-                CellContent.Sand => 'o',
-                _ => throw new ArgumentOutOfRangeException()
-            };
-            Console.Write(ch);
+            return currentSandPoint == sandFallingStartPoint;
         }
-        Console.WriteLine();
+
+        map[currentSandPoint.X, currentSandPoint.Y] = CellContent.Air;
+
+        var nextSandPoint = nextStepResult.Data;
+        map[nextSandPoint.X, nextSandPoint.Y] = CellContent.Sand;
+        currentSandPoint = nextSandPoint;
     }
 }
 
-
-Result<Point> TryGetSandNextStepPoint(CellContent[,] map, Point currentSandPoint)
+Result<Point> TryGetSandNextStepPoint(CellContent[,] map, int xSize, Point currentSandPoint)
 {
     var sandDirectionsPriority = new[]
     {
@@ -127,17 +108,27 @@ Result<Point> TryGetSandNextStepPoint(CellContent[,] map, Point currentSandPoint
     foreach (var fallingDirection in sandDirectionsPriority)
     {
         var nextPoint = currentSandPoint + fallingDirection;
-        if (map[nextPoint.X, nextPoint.Y] == CellContent.Air)
+        try
         {
-            return Result.Success(nextPoint);
+            if (nextPoint.X < 0 || nextPoint.X >= xSize)
+            {
+                return Result.Failure<Point>();
+            }
+
+            if (map[nextPoint.X, nextPoint.Y] == CellContent.Air)
+            {
+                return Result.Success(nextPoint);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
         }
     }
 
     return Result.Failure<Point>();
 }
-
-
-Console.WriteLine("Finish");
 
 Rule ParseRule(string line)
 {
@@ -145,12 +136,76 @@ Rule ParseRule(string line)
         .Select(ParsePoint)
         .ToArray();
     return new Rule(points);
+
+    Point ParsePoint(string str)
+    {
+        var (xStr, yStr) = str.Split(',');
+        return new Point(int.Parse(xStr), int.Parse(yStr));
+    }
 }
 
-Point ParsePoint(string str)
+CellContent[,] CreateMap(int maxX, int maxY, Rule[] rules)
 {
-    var (xStr, yStr) = str.Split(',');
-    return new Point(int.Parse(xStr), int.Parse(yStr));
+    Vector GetNormalizedVector(Vector vector)
+    {
+        return new Vector(Math.Sign(vector.X), Math.Sign(vector.Y));
+    }
+
+    void ApplyRule(CellContent[,] map, Rule rule)
+    {
+        var points = rule.Points;
+        var startPoint = points.First();
+        map[startPoint.X, startPoint.Y] = CellContent.Rock;
+
+        for (var j = 1; j < points.Length; j++)
+        {
+            var nextPoint = points[j];
+            var pointVector = nextPoint - startPoint;
+            var directionVector = GetNormalizedVector(pointVector);
+            var currentPoint = startPoint;
+            while (currentPoint != nextPoint)
+            {
+                currentPoint = currentPoint + directionVector;
+                map[currentPoint.X, currentPoint.Y] = CellContent.Rock;
+            }
+
+            startPoint = nextPoint;
+        }
+    }
+
+    void FillMap(CellContent[,] map, Rule[] rules)
+    {
+        foreach (var rule in rules)
+        {
+            ApplyRule(map, rule);
+        }
+    }
+
+    var cellContents = new CellContent[maxX + 1, maxY + 2]; // one for the abyss
+    FillMap(cellContents, rules);
+    return cellContents;
+}
+
+void PrintMap(CellContent[,] map, int maxX, int skipLeft, int addY = 0)
+{
+    for (var y = 0; y < maxY + 2 + addY; y++)
+    {
+        Console.Write(y.ToString().PadLeft(3, ' ') + ' ');
+
+        for (var x = skipLeft - 1; x < maxX + 1; x++)
+        {
+            var ch = map[x, y] switch
+            {
+                CellContent.Air => '.',
+                CellContent.Rock => '#',
+                CellContent.Sand => 'o',
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            Console.Write(ch);
+        }
+
+        Console.WriteLine();
+    }
 }
 
 enum CellContent
